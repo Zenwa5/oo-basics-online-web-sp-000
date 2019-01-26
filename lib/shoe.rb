385e365daf2e34d1b1b61ec9082a807c04a56aa6
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-01-26 20:19:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,17 @@
 # Make your shoe class here!
 class Shoe
 
-  def initialize(brand)
+  def initialize(brand, color)
     @brand = brand
+    @color = color
   end
 
   def brand
     @brand
+  end
+
+  def color(color)
+    @color
   end
 
 end
